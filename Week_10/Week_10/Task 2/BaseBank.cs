e2483540d99a42fbcef52727da5bf908416a28dd
Gocha Darjania <darjaniia@gmail.com>
2022-05-18 03:11:04 +0400

    Week 10 Homework - interface, abstract

---
+++ Week_10/Week_10/Task 2/BaseBank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_10.Task_2
+{
+    internal abstract class BaseBank
+    {
+        public int LoanAmount { get; set; }        
+        public int Months { get; set; }
+
+    }
+}
---
+++ Week_10/Week_10/Task 2/MicroFinance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_10.Task_2
+{
+    internal class MicroFinance : BaseBank, IFinanceOperations
+    {
+        public int CalculateLoanPercent(int month, int loanAmount)
+        {
+            var loanPercent = 10;
+            var monthAmount = loanAmount / month;//Monthly amount 
+            var montPercentAmount = monthAmount * loanPercent / 100;//amount of interest for 1 month 
+            var percenAmountSum = montPercentAmount * month;//Percent Sum 
+            var serviceAmountSum = month * 4;//Monthly service amount sum
+            var totalAmount = serviceAmountSum + percenAmountSum;
+
+            return totalAmount;
+        }
+
+        public bool CheckUserHistory()
+        {
+            return true;
+        }
+    }
+}
