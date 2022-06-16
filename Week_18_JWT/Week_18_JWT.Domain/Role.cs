b6d16aa00ea5c4ef30e790a4f1dce94e528b816c
Gocha Darjania <darjaniia@gmail.com>
2022-06-16 18:26:47 +0400

    Week 18, CRUD - JWT Rols Base auth

---
+++ Week_18_JWT/Week_18_JWT.Domain/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_18_JWT.Domain
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+        public ICollection<UserWithRole> UserWithRoles { get; set; }
+    }
+}
---
+++ Week_18_JWT/Week_18_JWT.Service/UserService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week_18_JWT.Data;
+using Week_18_JWT.Service.Interface;
+
+namespace Week_18_JWT.Service
+{
+    public class UserService : IUserService
+    {
+        UserDbContext _context;
+        public UserService(UserDbContext context)
+        {
+            _context = context;
+        }
+        public string UserLogin(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
